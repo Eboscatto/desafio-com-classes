fdc4765e30c42bbbf0702a77c47cf0b02ba1ed19
Eboscaatto <e_boscatto@hotmail.com>
2023-12-22 17:01:53 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//Bootcamp Potência Tech Dio - Programação do Zero
+//Terceiro desafio de projeto - escrevendo classes
+//Desenvolvido por: Everaldo Boscatto
+
+//Criando uma classe
+console.log("Inicialização de projeto")
+
+//Estanciando objeto
